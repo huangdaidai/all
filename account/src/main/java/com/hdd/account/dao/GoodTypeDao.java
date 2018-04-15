e2e54cb93defd453d9cf30db9236dc48cc692b3d
huangdaidai <214308645@qq.com>
2018-04-15 21:42:18 +0800

    修改代码生成器

---
+++ account/src/main/java/com/hdd/account/dao/GoodTypeDao.java
@@ -0,0 +1,13 @@
+package com.hdd.account.dao;
+
+import com.hdd.account.model.GoodType;
+import com.hdd.account.dao.GoodTypeDao;
+import org.springframework.stereotype.Repository;
+/**
+* author: hdd
+* 2018-4-15
+*/
+@Repository("goodTypeDao")
+public class GoodTypeDao extends BaseDao<GoodType>{
+   
+}
